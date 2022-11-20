05ab47f0739db44d4eaaf99927733379708b3c62
ohnas <ohnas12@gmail.com>
2022-11-20 18:21:22 +0900

    3.4 Room Detail

--- src/api.ts
+++ src/api.ts
@@ -4,5 +4,8 @@ const instance = axios.create({
     baseURL: "http://127.0.0.1:8000/api/v1/",
   });
   
-  export const getRooms = () =>
+export const getRooms = () =>
     instance.get("rooms/").then((response) => response.data);
+
+export const getRoom = () =>
+    instance.get(`rooms/11`).then((response) => response.data);
