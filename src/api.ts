0625bc6063d2ca40eecf4745198fe4e76297291a
ohnas <ohnas12@gmail.com>
2022-11-21 15:00:39 +0900

    4.4 CSRF

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,4 @@
+import Cookie from "js-cookie";
 import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -27,4 +28,10 @@ export const getMe = () =>
 // user/me/ -> 마지막에 '/' 있어야 오류 발생하지 않음 이유는 모르겠응 구굴링함
 
 export const logOut = () =>
-  instance.post(`users/log-out`).then((response) => response.data);
+  instance.post(`users/log-out/`, null, {
+      headers: {
+        "X-CSRFToken": Cookie.get("csrftoken") || "",
+      },
+    }).then((response) => response.data);
+// user/log-out -> 오류발생
+// user/log-out/ -> 마지막에 '/' 있어야 오류 발생하지 않음
