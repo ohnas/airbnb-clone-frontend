a9baa3d4b7d8752d82615cf4f4222c794198e458
ohnas <ohnas12@gmail.com>
2022-11-21 14:38:47 +0900

    4.3 Log Out

--- src/api.ts
+++ src/api.ts
@@ -11,7 +11,7 @@ export const getRooms = () =>
 
 export const getRoom = ({ queryKey }: QueryFunctionContext) => {
   const [_, roomPk] = queryKey;
-  return instance.get(`rooms/${roomPk}`).then((response) => response.data);
+  return instance.get(`rooms/${roomPk}/`).then((response) => response.data);
 };
 
 export const getRoomReviews = ({ queryKey }: QueryFunctionContext) => {
@@ -23,6 +23,8 @@ export const getRoomReviews = ({ queryKey }: QueryFunctionContext) => {
 
 export const getMe = () =>
   instance.get(`users/me/`).then((response) => response.data);
-
 // user/me -> 오류발생
 // user/me/ -> 마지막에 '/' 있어야 오류 발생하지 않음 이유는 모르겠응 구굴링함
+
+export const logOut = () =>
+  instance.post(`users/log-out`).then((response) => response.data);
