2d11b7108c9b5c5b0aa6cc8a32fbb08514342468
ohnas <ohnas12@gmail.com>
2022-11-22 14:23:34 +0900

    5.5 Upload Form

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -32,7 +32,7 @@ export default function RoomDetail() {
         {[0, 1, 2, 3, 4].map((index) => (
           <GridItem colSpan={index === 0 ? 2 : 1} rowSpan={index === 0 ? 2 : 1} overflow={"hidden"} key={index}>
             <Skeleton isLoaded={!isLoading} h="100%" w="100%">
-              {data?.photos && data.photos.length > 0 ? (
+              {data?.photos && data.photos.length > 4 ? (
                   <Image
                     objectFit={"cover"}
                     w="100%"
